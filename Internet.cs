cee65c38651fdeb75a2d70fbf89d79c76d8114a2
PROPHESSOR <xenteze@gmail.com>
2021-01-16 18:32:33 +0200

    Добавил методы для GET запросов и скачивания файлов

---
+++ Internet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DRRP_Launcher {
+    class Internet {
+        public static string Get(string url) {
+            var client = new WebClient();
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            client.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
+            return client.DownloadString(url);
+        }
+
+        public static object GetJson(string url) {
+            return JsonConvert.DeserializeObject(Get(url));
+        }
+
+        public static JArray GetJsonArray(string url) {
+            return JArray.Parse(Get(url));
+        }
+
+        public static JObject GetJsonObject(string url) {
+            return JObject.Parse(Get(url));
+        }
+
+        public static void Download(string url, string destination) {
+            var client = new WebClient();
+            client.DownloadFile(url, destination);
+        }
+    }
+}
